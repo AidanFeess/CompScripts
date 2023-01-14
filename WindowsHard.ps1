f0945807c20a3ba9a0088d7040eb101a57602472
VJMumphrey <vjm007@email.latech.edu>
2023-01-14 01:19:22 -0600

    improve the firewall and script flow

--- WindowsHard.ps1
+++ WindowsHard.ps1
@@ -29,8 +29,9 @@ function installtools($toolsPath, $curUsr) {
 }
 
 # once tools are run winpeas and parse the output and save it
-function toolstart($curUsr) {
+function toolstart($curUsr, $toolsPath) {
 	# open autoruns and procmon
+	Invoke-Expresision "& '"$toolsPath\sysinternals\procmon.exe""
 	
 	# run winpeas in the terminal
 	# from the README(github.com/carlospolop/PEASS-ng/tree/master/winPEAS/winPEASexe)
@@ -60,29 +61,30 @@ function winUP() {
 }
 
 # winfire only blocks certain ports at the moment
-function winFire(){
+function winFire($mode){
 	# turn the firewall on in all profiles
 	Set-NetFirewallProfile -Profile Domain,Public,Private -Enabled True
 
 	# turn defaults on and set logging
-	Set-NetFirewallRule -DefaultInboundAction Allow -DefaultOutboundAction Allow –LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
+	Set-NetFirewallProfile -DefaultInboundAction Allow -DefaultOutboundAction Allow -NotifyOnListen True -LogFileName %SystemRoot%\System32\LogFiles\Firewall\pfirewall.log
 
-	# # block all remote access on a system
-	# New-NetFirewallRule -DisplayName “Block Inbound Telnet” -Direction Inbound -LocalPort 23 -Action Block
-	# New-NetFirewallRule -DisplayName “Block Outbound Telnet” -Direction Outbound -LocalPort 23 -Action Block
-
-	# New-NetFirewallRule -DisplayName “Block Inbound msrpc” -Direction Inbound -LocalPort 135  -Action Block
-	# New-NetFirewallRule -DisplayName “Block Outbound msrpc” -Direction Outbound -LocalPort 135  -Action Block
-
-	# New-NetFirewallRule -DisplayName “Block Inbound rdp” -Direction Inbound -LocalPort 3389 -Action Block
-	# New-NetFirewallRule -DisplayName “Block Outbound rdp” -Direction Outbound -LocalPort 3389 -Action Block
-
-	# New-NetFirewallRule -DisplayName “Block Inbound ssh” -Direction Inbound -LocalPort 22 -Action Block
-	# New-NetFirewallRule -DisplayName “Block Outbound ssh” -Direction Outbound -LocalPort 22 -Action Block
+	# the only ports allowed at this point are
+	# - rdp
+	# - http/https
+	# - need to add any ftp and or smtp
 
 	# block all ports not in the list of safe ports
-	# TODO add port for ftp and smtp
-	$safeLs = @(22, 80, 443, 3389)
+	if ($mode == "lan") {
+
+		# TODO add previously listed ports here
+		$safeLs = @(80, 443)
+	}else{
+
+		# TODO same as line 79
+		$safeLs = @(80, 443, 3389)
+	}
+
+	$port = 0
 	ForEach($port in 5000) {
 		if ($port == $safeLs) {
 			continue;
@@ -94,7 +96,7 @@ function winFire(){
 }
 
 # open the ports that are requested
-function openPort($portNum, $action, $direction) {
+function editFirewallRule($portNum, $action, $direction) {
 	Set-NetFirewallRule -DisplayName "$action $direction $portNum" -Direction $direction -LocalPort $portNum -Action $action
 }
 
@@ -107,12 +109,6 @@ function changePass($curUsr) {
 }
 
 function main() {
-	[string]$curUsr = $env::Username
-	changePass $curUsr
-
-	# disable the old login accounts
-	net user quest /active:no
-
 	# list of tools to install
 	# winpeas, sysinternal suite
 	$toolsPath = "C:\Users\$curUsr\Tools\"
@@ -122,55 +118,121 @@ function main() {
 
 		if (Get-ChildItem -Path "C:\Users\$curUsr\Tools\" -Recurse | Measure-Object == 0) {
 
-		install-tools ($toolsPath)
+			install-tools ($toolsPath)
 		}
 	}
 
-	toolstart($curUsr)
+	Write-Output "[+] choose a mode to run the script"
+	Start-Sleep -Milliseconds 500
+	Write-Output "[+] harden will start the hardening process on the current machine"
+	Start-Sleep -Milliseconds 500
+	Write-Output "[+] control will allow the user to make changes to windows without having to navigate around"
+	Start-Sleep -Milliseconds 500
+	$usermode = Read-Host -Prompt "(Harden) or (Control)"
+	if ($usermode == "harden" or "Harden") {
 
-	# if ActiveDirectory
-	$ActiveDirectoryPresent = 
-	if ($ActiveDirectoryPresent == True) {
+		# disable the old login accounts
+		net user quest /active no
+		# harden the firewall for remote or lan comps
+		$winFirewallOn = Read-Host -Prompt "Do you want to turn on the windows firewall (y)"
+		if ($winFirewallOn == "y") {
+			$mode = Read-Host -Prompt "lan or remote (lan) or (remote)"
+			winFire ($mode)
+		}
+
+		# if ActiveDirectory
 		$hardenAD = Read-Host -Prompt "Do you want to Harden AD (y)"
 		if ($hardenAD == "y") {
 			ADHard
 		}
-	}
 
-	$ExchangePresent = 
-	if ($ExchangePresent == True) {
 		$hardenExch = Read-Host -Prompt "Do you want to Harden Exchange (y)"
 		if ($hardenExch == "y") {
 			ExchangeHard
 		}
-	}
 
-	# remove the tools directory once finished with recon
-	$removeTools = Read-Host -Prompt "Do you want to remvove the tools folder (n)"
-	if ($removeTools == "y") {
-		Remove-Item -LiteralPath "$toolsPath" -Force -Recurse
-	}
-	
-	# turn on Windows Defender
-	$turnDefenderOn = Read-Host -Prompt "Do you want to turn on Windows Defender (y)"
-	# Windows 8.1 (server 2016+) should already be on
-	# pulled from(https://support.huntress.io/hc/en-us/articles/4402989131283-Enabling-Microsoft-Defender-using-Powershell-)
-	# need to test
-	if ($turnDefenderOn == "y") {
-		Set-MpPreference -DisableRealtimeMonitoring $false
-		Set-MpPreference -DisableIOAVProtection $false
-		New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "Real-Time Protection" -Force
-		New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableBehaviorMonitoring" -Value 0 -PropertyType DWORD -Force
-		New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableOnAccessProtection" -Value 0 -PropertyType DWORD -Force
-		New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableScanOnRealtimeEnable" -Value 0 -PropertyType DWORD -Force
-		New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 0 -PropertyType DWORD -Force
-		start-service WinDefend
-		start-service WdNisSvc	
-	}
-	
-	# maybe perform windows updates?(Rules permitting)
-	$updates = Read-Host -Prompt "Do you want to update (y)"
-	if ($updates == True) {
-		winUP
+		# turn on Windows Defender
+		$turnDefenderOn = Read-Host -Prompt "Do you want to turn on Windows Defender (y)"
+		# Windows 8.1 (server 2016+) should already be on
+		# pulled from(https://support.huntress.io/hc/en-us/articles/4402989131283-Enabling-Microsoft-Defender-using-Powershell-)
+		# need to test
+		if ($turnDefenderOn == "y") {
+			Set-MpPreference -DisableRealtimeMonitoring $false
+			Set-MpPreference -DisableIOAVProtection $false
+			New-Item -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "Real-Time Protection" -Force
+			New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableBehaviorMonitoring" -Value 0 -PropertyType DWORD -Force
+			New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableOnAccessProtection" -Value 0 -PropertyType DWORD -Force
+			New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender\Real-Time Protection" -Name "DisableScanOnRealtimeEnable" -Value 0 -PropertyType DWORD -Force
+			New-ItemProperty -Path "HKLM:\SOFTWARE\Policies\Microsoft\Windows Defender" -Name "DisableAntiSpyware" -Value 0 -PropertyType DWORD -Force
+			start-service WinDefend
+			start-service WdNisSvc	
+		}
+
+		toolstart($curUsr, $toolsPath)
+
+		# remove the tools directory once finished with recon
+		$removeTools = Read-Host -Prompt "Do you want to remvove the tools folder (n)"
+		if ($removeTools == "y") {
+			Remove-Item -LiteralPath "$toolsPath" -Force -Recurse
+		}
+		
+		# maybe perform windows updates?(Rules permitting)
+		$updates = Read-Host -Prompt "Do you want to update (y)"
+		if ($updates == True) {
+			winUP
+		}
+
+		# change the password
+		Write-Host "[+] you are about to change your password"
+		[string]$curUsr = $env::Username
+		changePass $curUsr
+		Write-Host "[+] MAKE SURE TO LOGOUT AND LOG BACK IN FOR THE CHANGE TO TAKE EFFECT"
+
+		# change the execution policy for powershell for admins only (works for the current machine)
+		# rest of restrictions happen in group policy and active directory
+		Set-ExecutionPolicy -ExecutionPolicy Restricted -Scope LocalMachine
+	}else{
+		while(True) {
+			Write-Host "[+] what would you like to do
+			- edit a firewall rule(1)
+			- change a group policy(2)
+			- edit active directory(3)
+			- quit(4)
+			"
+			$choice = Read-Host 
+			switch ($choice) {
+
+				condition1 {$choice == "1"}
+				condition1 {
+					$portNum = Read-Host -Prompt "which port (num)"
+					$action = Read-Host -Prompt "(allow) or (block)"
+					$direction = Read-Host -Prompt "which direction (in) or (out)"
+					editFirewallRule ($portNum, $action, $direction)
+				}
+
+				condition2 {$choice == "2"}
+				condition2 {
+
+					# TODO populate this with stuff after group policy is added
+
+				}
+
+				condition3 {$choice == "3"}
+				condition3 {
+
+					# TODO populate this with stuff after group policy is added
+
+				}
+
+				condition4 {$choice == "4"}
+				condition4 {
+					break
+				}
+
+				default {
+					continue
+				}
+			}
+		}
 	}
 }
