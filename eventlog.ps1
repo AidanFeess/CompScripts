3ec24dac1a4c8020f8b6117919db118f20542fb0
VJMumphrey <vjm007@latech.edu>
2023-04-14 09:43:06 -0500

    small tests to wonk features

--- eventlog.ps1
+++ eventlog.ps1
@@ -1,6 +1,7 @@
 # read the security event log for logons and logoffs output to a csv
-$result = Get-EventLog -LogName Security -InstanceId 4624, 4647 -Newest 10 | 
-ForEach-Object {
+$results = [System.Collections.Generic.List[object]]::new()
+$result = Get-EventLog -LogName Security -InstanceId 4624, 4647 -Newest 10
+$results = foreach($obj in $result) {
     [PSCustomObject]@{
     Time = $_.TimeGenerated
     # Machine = $_.ReplacementStrings[6]
@@ -8,16 +9,19 @@ ForEach-Object {
     Access = $_.ReplacementStrings[10]
     # SourceAddr = $_.ReplacementStrings[18]
     }
+
+    $results.AddRange((obj))
+    
 }
 
-# TODO system that scans the csv to update the info that is already there
+# TODO system that scans the csv to update the info with current results
 
-$result | Select-Object Time, User, Access | Export-Csv -NoTypeInformation -Path .\Access_Log.csv
+$results | Select-Object Time, User, Access | Export-Csv -NoTypeInformation -Path .\Access_Log.csv
 
 # logoff all users
-Invoke-CimMethod -ClassName Win32_Operatingsystem -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
+# Invoke-CimMethod -ClassName Win32_Operatingsystem -MethodName Win32Shutdown -Arguments @{ Flags = 4 }
 
 # logoff specfic users (from Windows 2016 version of powershell)
-$sessions = Get-RDUserSession
+$sessions = Get-RDUserSession | Select-Object -Pattern "$badusername"
 $sessionId = ((quser | Where-Object { $_ -match $userName }) -split ' +')[2]
 Invoke-RDUserLogoff $sessionId
